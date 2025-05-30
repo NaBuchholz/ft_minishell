7c8739d9c5ffe664014d5ae42a41faf1816f21bf
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-05-30 17:07:58 -0300

    fix: Update header files and Makefile for consistency and improved readability

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,14 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 10:59:25 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/05/27 11:06:38 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/05/30 16:56:02 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include "../libft/libft.h"
+# include "libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
 #endif
