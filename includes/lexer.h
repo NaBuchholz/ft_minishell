306e3c08d71ec9d3f3a626d0218e8a825ee2c0cc
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-05-31 14:57:30 -0300

    feat: Add lexer.h header file with token type and structure definitions

---
+++ includes/lexer.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 14:00:53 by nbuchhol          #+#    #+#             */
+/*   Updated: 2025/05/31 14:23:35 by nbuchhol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+# include "minishell.h"
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT
+}					t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}					t_token;
+
+#endif
