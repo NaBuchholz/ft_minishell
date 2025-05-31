28cbbb6d52b1605c11356bbb5c63bf9cd7829b96
Nathalia Buchholz <nathaliabuchh@hotmail.com>
2025-05-30 22:34:46 -0300

    Definir estruturas principais (#61)

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 10:59:25 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/05/30 16:56:02 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/05/30 22:17:04 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,27 @@
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT
+}					t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}					t_token;
+
+typedef struct s_shell
+{
+	char			*input;
+	t_token			*tokens;
+	int				exit_status;
+}					t_shell;
+
 #endif
