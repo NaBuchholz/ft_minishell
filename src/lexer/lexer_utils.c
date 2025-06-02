06740a11da170747332d17f5fd92c50581460fef
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-06-02 14:49:45 -0300

    feat: enhance lexer with word handling and metacharacter checks

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 11:59:48 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/06/01 13:37:16 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/06/02 14:39:49 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,24 @@ int	skip_whitespace(char *input, int pos)
 	return (pos);
 }
 
+/**
+ * @brief Check if character is a shell operator metacharacter.
+ * @param c Character to check.
+ * @return 1 if operator, 0 otherwise.
+ */
 int	is_operator(char c)
 {
 	return (c == '|' || c == '<' || c == '>');
+}
+
+/**
+ * @brief Check if character is a metacharacter (operator or whitespace).
+ * @param c Character to check.
+ * @return 1 if metacharacter, 0 otherwise.
+ */
+int	is_metacharacter(char c)
+{
+	return (ft_isspace(c) || is_operator(c));
 }
 
 /**
@@ -45,4 +60,20 @@ t_token_type	get_operator_type(char *input, int pos)
 	if (input[pos] == '>')
 		return (TOKEN_REDIR_OUT);
 	return (TOKEN_WORD);
+}
+
+/**
+ * @brief Calculate the length of a word token.
+ * @param input Input string.
+ * @param start Starting position in string.
+ * @return Length of the word (number of characters).
+ */
+int	get_word_length(char *input, int start)
+{
+	int	len;
+
+	len = 0;
+	while (input[start + len] && !is_metacharacter(input[start + len]))
+		len++;
+	return (len);
 }
