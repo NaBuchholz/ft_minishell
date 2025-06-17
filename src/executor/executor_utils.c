46a7ff45ccef0336e07079dfc3663d9b50af4ae2
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-06-17 14:16:16 -0300

    feat: Add executor header and utility functions for command execution

---
+++ src/executor/executor_utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 09:50:04 by nbuchhol          #+#    #+#             */
+/*   Updated: 2025/06/17 14:16:03 by nbuchhol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "executor.h"
+
+char	**cmd_to_argv(t_cmd *cmd)
+{
+	char	**argv;
+	t_arg	*current;
+	int		i;
+
+	if (!cmd || !cmd->args)
+		return (NULL);
+	argv = ft_calloc(cmd->arg_count + 1, sizeof(char *));
+	if (!argv)
+		return (NULL);
+	i = 0;
+	current = cmd->args;
+	while (current)
+	{
+		argv[i++] = ft_strdup(current->value);
+		current = current->next;
+	}
+	return (argv);
+}
+
+void	free_argv(char **argv)
+{
+	int	i;
+
+	if (!argv)
+		return ;
+	i = 0;
+	while (argv[i])
+		free(argv[i++]);
+	free(argv);
+}
