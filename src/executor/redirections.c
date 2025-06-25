75f8269c0093029d32b4e25ed9cdfcc27088782b
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-06-25 13:20:29 -0300

    fix: implement append redirection handling

--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 20:36:20 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/06/25 12:55:06 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/06/25 13:20:00 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,28 @@ static int	handle_out_redir(char *filename)
 		return (-1);
 	}
 	new_fd = dup2(file_fd, 1);
+	if (new_fd == -1)
+	{
+		perror("minishell: dup2\n");
+		close(file_fd);
+		return (-1);
+	}
+	close(file_fd);
+	return (0);
+}
+
+static int	handle_in_redir(char *filename)
+{
+	int	file_fd;
+	int	new_fd;
+
+	file_fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (file_fd == -1)
+	{
+		perror("minishell: open");
+		return (-1);
+	}
+	new_fd = dup2(file_fd, 0);
 	if (new_fd == -1)
 	{
 		perror("minishell: dup2\n");
@@ -62,8 +84,8 @@ static int	handle_redir_by_type(t_token_type type, char *target)
 		return (handle_out_redir(target));
 	else if (type == TOKEN_REDIR_IN)
 		return (handle_in_redir(target));
-//   else if (type == TOKEN_REDIR_APPEND)
-//       return (handle_append_redir(target));  // Para quando implementar >>
+	else if (type == TOKEN_REDIR_APPEND)
+		return (handle_append_redir(target));
 //    else if (type == TOKEN_HEREDOC)
 //       return (handle_heredoc_redir(target)); // Para quando implementar
 	return (0);
