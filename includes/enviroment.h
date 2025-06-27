91287303865647fda98bef9fd5d96e21510a17b8
Vinicius Da Silva Lima <vinda-si@c1r6p1.42sp.org.br>
2025-06-26 21:03:20 -0300

    change enviroment.h

--- includes/enviroment.h
+++ includes/enviroment.h
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 15:55:10 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/06/26 19:14:46 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/06/26 21:02:26 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "lexer.h"
 
 int	expand_token_variables(t_token *tokens, char **env, int status);
-int		builtin_env(char **argv, char **envp);
+int	builtin_env(char **argv, char **envp);
 
 #endif
